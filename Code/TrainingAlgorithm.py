e410b488453b5c5e85b467d9c271930443329fa6
ScreamingMole <tobi.penter@btinternet.com>
2025-02-11 11:32:19 +0000

    Removed old data parser. Added SMOTE to training and made the code work for an MLP

--- Code/TrainingAlgorithm.py
+++ Code/TrainingAlgorithm.py
@@ -2,36 +2,38 @@ import random
 import numpy as np
 import pandas as pandas
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error, mean_squared_error, precision_score, r2_score, recall_score
 import pickle
+from imblearn.over_sampling import SMOTE
 import shap
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_agg as agg
 
 # Load data
-trainingData = pandas.read_csv("Data for ML/trainingData.csv")
-
-trainingData = pandas.DataFrame(trainingData.values, columns=trainingData.columns)
+trainingData = pandas.read_csv("Data for ML/UNSW-NB15/UNSW-NB15_DataFrame.csv")
 
 # Define the inputs and targets
-trainingData = trainingData.sample(n=20000, random_state=42)
 input = trainingData.drop(['IPV4_SRC_ADDR', 'IPV4_DST_ADDR','Label', 'Attack'], axis=1)
-target = trainingData['Attack'].values.astype(float)
+target = trainingData['attack_cat'].values.astype(float)
 
 # Data split
-input_training, input_test, target_training, target_test = train_test_split(input, target, test_size=0.2, random_state=42)
+input_training, input_test, target_training, target_test = train_test_split(input, target, test_size=0.1, random_state=42)
+
+# Use SMOTE to balance
+smote = SMOTE(sampling_strategy='auto', random_state=42)
+input_training, target_training = smote.fit_resample(input_training, target_training)
 
 # Not using an Random Forest for project but it is being used as a test
-rfcAttack = RandomForestClassifier(n_estimators=70, max_depth=80, bootstrap=True, min_samples_split=2, min_samples_leaf=1, random_state=42)
-rfcAttack.fit(input_training, target_training)
+MLPAttackClassifier = MLPClassifier(hidden_layer_sizes=(256, 128, 64, 16), activation='relu', solver='adam', max_iter=500, random_state=42, early_stopping=True)
+MLPAttackClassifier.fit(input_training, target_training)
 
 AllPredictions= []
 AllAttacks = []
 
 #Test model and generate statistics
 for i in range(5):
-    attackPrediction = rfcAttack.predict(input_test)
+    attackPrediction = MLPAttackClassifier.predict(input_test)
     AllPredictions.append(attackPrediction)
     AllAttacks.append(target_test)
     
@@ -65,39 +67,32 @@ r2 = r2_score(AllAttacks, AllPredictions)
 print("Total Accuracy Score: ",accuracy,"\nTotal Precision Score: ", precision,"\nTotal Recall Score:", recall,"\nTotal F1 Score: ", f1,"\nTotal Mean Square Error: ", errorMean,"\nTotal Mean Absolute Error: ", errorAbsolute,"\nTotal R2 Score: ",r2)
 
 #Save model
-pickle.dump(rfcAttack, open("Models/rfcAttackModel.pk1", 'wb'))
+pickle.dump(MLPAttackClassifier, open("Models/MLPModel.pk1", 'wb'))
 
 #Initialise shap plot
 shap.initjs()
 
 #Make explainer
-explainer = shap.TreeExplainer(rfcAttack)
+explainer = shap.KernelExplainer(MLPAttackClassifier)
 
 shap_values = explainer.shap_values(input_test)
 
-print(shap_values.shape)
-
 shap_values = np.transpose(shap_values, (2,0,1))
-
-print(shap_values.shape)
 
 #Shape values for each class
 for i, class_shap_values in enumerate(shap_values):
-    print(class_shap_values.shape)
-    print(input_test.shape)
     print(f"Generating summary plot for class {i}")
     summary_plot = shap.summary_plot(class_shap_values, input_test, feature_names=input_test.columns, show=False)
     
     fig = plt.gcf()
 
-    # Use agg backend to save it as PNG
+    #Use agg backend to save it as PNG
     png_file_path = f"Explainer Charts/Summary/On Build/Model 1/shap_plot_class{i}.png"
     agg_backend = agg.FigureCanvasAgg(fig)
     agg_backend.print_png(png_file_path)
-
-    # Optionally close the figure to avoid memory issues
     plt.close(fig)
 
+#Shap values for 10 different predicitons
 for j in range (10):
     instance = random.randint(0, len(input_test) - 1)
 
