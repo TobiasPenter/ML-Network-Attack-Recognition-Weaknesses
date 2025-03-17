5715a710065cda7e7906db087dcc4e403f7a239a
TobiasPenter <tobi.penter@gmail.com>
2025-03-17 12:09:15 +0000

    made separate testing files for each model, created the ann/mlp model and moved shap diagram generation to the testing files

--- Code/LightGBMTesting.py
+++ Code/LightGBMTesting.py
@@ -6,22 +6,22 @@ import shap
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score, mean_absolute_error, mean_squared_error, precision_score, r2_score, recall_score
 import pickle
+import matplotlib.backends.backend_agg as agg
 
 # Load data
-trainingData = pandas.read_csv("Data for ML/trainingData.csv")
+trainingData = pandas.read_csv("Data for ML/UNSW-NB15/UNSW-NB15_DataFrame.csv")
 
 trainingData = pandas.DataFrame(trainingData.values, columns=trainingData.columns)
 
 # Define the inputs and targets
 trainingData = trainingData.sample(n=200000, random_state=42)
-input = trainingData.drop(['IPV4_SRC_ADDR', 'IPV4_DST_ADDR','Label', 'Attack'], axis=1)
-target = trainingData['Attack'].values.astype(float)
+target = trainingData['attack_cat'].values.astype(float)
 
 # Data split
-input_training, input_test, target_training, target_test = train_test_split(input, target, test_size=0.2, random_state=42)
+input_training, input_test, target_training, target_test = train_test_split(trainingData.drop(columns='attack_cat'), target, test_size=0.2, random_state=42)
 
 #Load Model
-model_For_Testing = pickle.load(open('Models/rfcAttackModel.pk1', 'rb'))
+model_For_Testing = pickle.load(open('Models/MLPModel.pk1', 'rb'))
 
 AllPredictions= []
 AllAttacks = []
@@ -44,7 +44,7 @@ for i in range(5):
     
     #Resplit data
     ran_state_val = random.randint(30, 70)
-    input_training, input_test, target_training, target_test = train_test_split(input, target, test_size=0.2, random_state=ran_state_val)
+    input_training, input_test, target_training, target_test = train_test_split(trainingData.drop(columns='attack_cat'), target, test_size=0.2, random_state=ran_state_val)
    
 #Overall statisticss 
 AllAttacks = np.concatenate(AllAttacks, axis=0)
@@ -59,20 +59,23 @@ errorAbsolute = mean_absolute_error(AllAttacks, AllPredictions)
 r2 = r2_score(AllAttacks, AllPredictions)
 
 print("Total Accuracy Score: ",accuracy,"\nTotal Precision Score: ", precision,"\nTotal Recall Score:", recall,"\nTotal F1 Score: ", f1,"\nTotal Mean Square Error: ", errorMean,"\nTotal Mean Absolute Error: ", errorAbsolute,"\nTotal R2 Score: ",r2)
+
 #Initialise shap plot
 shap.initjs()
 
 #Make explainer
-explainer = shap.TreeExplainer(model_For_Testing)
+background_sample = shap.sample(input_test, 10000)
 
-shap_values = explainer.shap_values(input_test)
+explainer = shap.Explainer(model_For_Testing.predict_proba, background_sample)
 
-shap_values = np.transpose(shap_values, (2,0,1))
+shap_values = explainer.shap_values(background_sample)
+
+shap_values = np.transpose(shap_values.transpose(1, 0, 2))
 
 #Shape values for each class
 for i, class_shap_values in enumerate(shap_values):
     print(f"Generating summary plot for class {i}")
-    summary_plot = shap.summary_plot(class_shap_values, input_test, feature_names=input_test.columns, show=False)
+    summary_plot = shap.summary_plot(class_shap_values, background_sample, feature_names=background_sample.columns, show=False)
     
     fig = plt.gcf()
 
@@ -81,14 +84,3 @@ for i, class_shap_values in enumerate(shap_values):
     agg_backend = agg.FigureCanvasAgg(fig)
     agg_backend.print_png(png_file_path)
     plt.close(fig)
-
-#Shap values for 10 different predicitons
-for j in range (10):
-    instance = random.randint(0, len(input_test) - 1)
-
-    for i, class_shap_values in enumerate(shap_values):  
-        shap_instance = class_shap_values[instance]
-        print(f"Generating plot for instance {instance}, class {i}")
-        force_plot = shap.force_plot(explainer.expected_value[i], shap_instance, input_test.iloc[instance].values, feature_names=input_test.columns, show=False)
-
-        shap.save_html(f"Explainer Charts/Instances/On Build/Model 1/Iteration {j}/shap_plot_class{i}_instance{instance}.html", force_plot)
